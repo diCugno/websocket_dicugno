d5a6ec0c5ad45839ca010dfb902014c308a241a4
diCugno <dialessandro@itis-molinari.eu>
2025-04-05 15:58:13 +0200

    script aggiornato

--- public/script.js
+++ public/script.js
@@ -2,30 +2,63 @@ const input = document.getElementById("input");
 const button = document.getElementById("sendButton");
 const chat = document.getElementById("chat");
 
-const socket = io();
+const nameInput = document.getElementById("nameInput");
+const saveNameBtn = document.getElementById("saveNameBtn");
+
+const template = "<li class=\"list-group-item\">%MESSAGE</li>";
 const messages = [];
 
-const template = '<li class="list-group-item">%MESSAGE</li>';
+const socket = io();
+let userName = "";
 
-button.onclick = () => {
-  socket.emit("message", input.value);
-  input.value = "";
-}
+//mostra modale
+const nameModal = new bootstrap.Modal(document.getElementById('nameModal'), {
+  backdrop: 'static',
+  keyboard: false
+});
+nameModal.show();
+
+//salva il nome e chiude
+saveNameBtn.onclick = () => {
+  const name = nameInput.value;
+  if (name !== "") {
+    userName = name;
+    nameModal.hide();
+  }
+};
 
 input.onkeydown = (event) => {
-  if (event.key === 'Enter') {
+  if (event.key === "Enter") {
     event.preventDefault();
     button.click();
   }
-}
+};
 
-socket.on("chat", (message) => {
-  console.log(message);
+button.onclick = () => {
+  const text = input.value;
+  if (userName && text !== "") {
+    socket.emit("message", {
+      name: userName,
+      text: text
+    });
+    input.value = "";
+  }
+};
+
+//ricezione
+socket.on("chat", (data) => {
+  const { name, text } = data;
+  const message = `${name}: ${text}`;
   messages.push(message);
   render();
 });
 
 const render = () => {
-  chat.innerHTML = messages.map(m => template.replace("%MESSAGE", m)).join("");
+  let html = "";
+  messages.forEach((message) => {
+    const row = template.replace("%MESSAGE", message);
+    html += row;
+  });
+  chat.innerHTML = html;
   window.scrollTo(0, document.body.scrollHeight);
 };
--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const fs = require('fs');
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
-const conf = JSON.parse(fs.readFileSync('./conf.json'));
-
+const express = require("express");
+const http = require("http");
 const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const { Server } = require('socket.io');
+
+const conf = JSON.parse(fs.readFileSync("./conf.json"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", express.static(path.join(__dirname, "public")));
+
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-
 io.on('connection', (socket) => {
   console.log("socket connected: " + socket.id);
-  io.emit("chat", "new client: " + socket.id);
-
-  socket.on('message', (message) => {
-    const response = socket.id + ': ' + message;
-    console.log(response);
-    io.emit("chat", response);
+  
+  //quando un messaggio arriva dal client
+  socket.on('message', (data) => {
+    const response = {
+      name: data.name,
+      text: data.text
+    };
+    console.log(`${response.name}: ${response.text}`);
+    io.emit("chat", response); // lo manda a tutti
   });
 
   socket.on('disconnect', () => {
